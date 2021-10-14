21b6867e579a66548e7427784c549ec9f01bea4e
Scarlet <66797411+TanZeus@users.noreply.github.com>
2021-10-14 18:14:31 +0530

    Bug fixes

--- Snake.py
+++ Snake.py
@@ -2,10 +2,10 @@ import pygame
 import time
 import random
 
-snake_speed = 15
+ss = 15
 
-window_x = 720
-window_y = 480
+winx = 720
+winy = 480
 
 black = pygame.Color(0 , 0 , 0)
 white = pygame.Color(255 , 255 , 255)
@@ -16,18 +16,18 @@ blue = pygame.Color(0, 0, 255)
 
 pygame.init()
 pygame.display.set_caption('Snakes')
-game_window = pygame.display.set_mode((window_x, window_y))
+game_window = pygame.display.set_mode((winx, winy))
 fps = pygame.time.Clock()
-snake_position = [100 , 50]
+sp = [100 , 50]
 
 # defining first 4 blocks of snake body
-snake_body = [  [100 , 50] ,
+snb = [  [100 , 50] ,
                 [90 , 50] ,
                 [80 , 50] ,
                 [70 , 50]
             ]
-fruit_position = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
-fruit_spawn = True
+fr_pos = [random.randrange(1, (winx//10)) * 10, random.randrange(1, (winy//10)) * 10]
+fr_spwn = True
 # setting default snake direction
 direction = 'RIGHT'
 change_to = direction
@@ -45,7 +45,7 @@ def game_over():
     my_font = pygame.font.SysFont('times new roman', 50)
     game_over_surface = my_font.render('Your Score is : ' + str(score) , True , red)
     game_over_rect = game_over_surface.get_rect()
-    game_over_rect.midtop = (window_x/2, window_y/4)
+    game_over_rect.midtop = (winx/2, winy/4)
     game_window.blit(game_over_surface, game_over_rect)
     pygame.display.flip()
     time.sleep(2)
@@ -79,42 +79,42 @@ while True:
  
     # Moving the snake
     if direction == 'UP':
-        snake_position[1] -= 10
+        sp[1] -= 10
     if direction == 'DOWN':
-        snake_position[1] += 10
+        sp[1] += 10
     if direction == 'LEFT':
-        snake_position[0] -= 10
+        sp[0] -= 10
     if direction == 'RIGHT':
-        snake_position[0] += 10
+        sp[0] += 10
  
     # Snake body growing mechanism if fruits and snakes collide then scores will be incremented by 10
-    snake_body.insert(0, list(snake_position))
-    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+    snb.insert(0, list(sp))
+    if sp[0] == fr_pos[0] and sp[1] == fr_pos[1]:
         score += 10
-        fruit_spawn = False
+        fr_spwn = False
     else:
-        snake_body.pop()
+        snb.pop()
          
-    if not fruit_spawn:
-        fruit_position = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
+    if not fr_spwn:
+        fr_pos = [random.randrange(1, (winx//10)) * 10, random.randrange(1, (winy//10)) * 10]
          
-    fruit_spawn = True
+    fr_spwn = True
     game_window.fill(black)
      
-    for pos in snake_body:
+    for pos in snb:
         pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
          
-    pygame.draw.rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+    pygame.draw.rect(game_window, white, pygame.Rect(fr_pos[0], fr_pos[1], 10, 10))
  
     # Game Over conditions
-    if snake_position[0] < 0 or snake_position[0] > window_x-10:
+    if sp[0] < 0 or sp[0] > winx-10:
         game_over()
-    if snake_position[1] < 0 or snake_position[1] > window_y-10:
+    if sp[1] < 0 or sp[1] > winy-10:
         game_over()
 
-    for block in snake_body[1:]:
-        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+    for block in snb[1:]:
+        if sp[0] == block[0] and sp[1] == block[1]:
             game_over()
     show_score(1, white, 'times new roman', 20)
     pygame.display.update()
-    fps.tick(snake_speed)
+    fps.tick(ss)
